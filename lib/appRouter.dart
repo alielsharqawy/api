caecf87cde67a2fa5c95b25dd4c0ec6328567659
ali <140660389+alielsharqawy@users.noreply.github.com>
2024-03-07 01:21:10 +0200

    final

--- lib/appRouter.dart
+++ lib/appRouter.dart
@@ -1,12 +1,12 @@
 // ignore_for_file: body_might_complete_normally_nullable
-import 'package:api/loginScreen.dart';
+import 'package:api/screens/loginScreen.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   Route? generationRouter(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        return MaterialPageRoute(builder: (_) => const SignInScreen());
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:api/appRouter.dart';
 import 'package:flutter/material.dart';
 
