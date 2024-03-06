6b001028ca6492eb52feaea9f060d157fc765b86
ali <140660389+alielsharqawy@users.noreply.github.com>
2024-03-07 00:41:09 +0200

    first commit

---
+++ lib/appRouter.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: body_might_complete_normally_nullable
+import 'package:api/loginScreen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route? generationRouter(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:api/appRouter.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( Api(appRouter: AppRouter(),));
+}
+class Api extends StatelessWidget {
+final AppRouter appRouter;
+  const Api({super.key, required this.appRouter, });
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute:appRouter.generationRouter,
+    );
+  }
+}
